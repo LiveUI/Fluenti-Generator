dcf22ae0252e4d9e9a7f41f9c2ee86467d5d4083
Ondrej Rafaj <ondrej.rafaj@gmail.com>
2018-06-16 21:32:46 +0100

    wip

--- generate.sh
+++ generate.sh
@@ -5,8 +5,16 @@
 #cd ./SPM && vapor clean -y --verbose && rm -f Package.resolved && rm -rf .build && vapor xcode -n --verbose
 
 
-# Clean Fluenti folder, all but .git
-cd ./Fluenti && find . ! -name '.git' -type f -exec rm -f {} +
-cp ../Resources/gitignore .gitignore
-cp ../Resources/LICENSE LICENSE
-cp ../Resources/README.md README.md 
+# Clean Fluenti folder, all but essentials
+cd ./Fluenti
+
+mkdir -p ../.tmp
+
+cp .gitignore ../.tmp/.gitignore
+cp LICENSE ../.tmp/LICENSE
+cp README.md ../.tmp/README.md
+
+find . ! -name '.git' -type f -exec rm -f {} +
+cp ../.tmp/.gitignore .gitignore
+cp ../.tmp/LICENSE LICENSE
+cp ../.tmp/README.md README.md
---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# Fetch Fluenti submodule
+git submodule update --recursive
