8988b8a503f4f4b0ddb13e2d85398cde09164878
Ondrej Rafaj <ondrej.rafaj@gmail.com>
2018-06-16 21:23:19 +0100

    resources

--- generate.sh
+++ generate.sh
@@ -2,12 +2,11 @@
 
 # Update SPM folder (original code)
 
-cd ./SPM &&\
-	vapor clean -y --verbose &&\
-	rm -f Package.resolved &&\
-	rm -rf .build&&\
-	vapor xcode -n --verbose
+#cd ./SPM && vapor clean -y --verbose && rm -f Package.resolved && rm -rf .build && vapor xcode -n --verbose
 
 
 # Clean Fluenti folder, all but .git
-#cd ./SPM && find . ! -name '.git' -type f -exec rm -f {} +
+cd ./Fluenti && find . ! -name '.git' -type f -exec rm -f {} +
+cp ../Resources/gitignore .gitignore
+cp ../Resources/LICENSE LICENSE
+cp ../Resources/README.md README.md 
